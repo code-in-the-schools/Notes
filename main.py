82e11833c6e3905f2f7ee82770d405335a8faeaf
BenjaminKing39 <bekingly39@gmail.com>
2020-07-27 17:46:29 +0000

    First Day

---
+++ main.py
@@ -0,0 +1,55 @@
+
+#def oof(word, otherword):
+  #print(word + " " + otherword)
+
+#oof("Sand", "Man")
+#oof("Left","Shake")
+
+#___________________________________
+
+#This Function Return With
+#def addone(i):
+  #return(i+1)
+
+#def addtwo(i):
+  #return(i+2)
+
+#def mynamenoa(name):
+  #newname = ''
+  #for i in range(len(name)):
+    #if name[i] != 'a' and name [i]!= "A":
+      #newname += name[i] 
+  #return newname
+
+#n = str(input("what's your name"))
+
+#a = int(input("number: "))
+
+#print(addone(a))
+#print(addtwo(a))
+
+#b = addone(a) = addtwo(a)
+
+#print('B:' , b)
+
+#_________________________________
+
+#def mynamenoa(name):
+  #newname = ''
+  #l = str(letter)
+
+  #for i in range(len(name)):
+    #if name[i] != letter .letter and name[i]!= letter .upper:
+      #newname += name[i] 
+  #return newname
+
+#running = True
+
+#while running:
+
+  #n = str(input('name:'))
+  #l = str(input('letter'))
+
+ #if(str(mynamenoa(n,l)) == ss
+
+  #print(mynamenoa(n,l))
